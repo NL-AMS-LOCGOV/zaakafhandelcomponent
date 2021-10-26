ee4727b6c9981f2cf10900c43128c6ac8bf653c6
Andy Verberne <andy.verberne@atos.net>
2021-10-26 13:47:02 +0200

    ESUITEDEV-25826 Verder onderzoek naar trage werkvoorraad

--- src/main/java/net/atos/client/zgw/shared/InvocationBuilderFactory.java
+++ src/main/java/net/atos/client/zgw/shared/InvocationBuilderFactory.java
@@ -9,12 +9,16 @@ import static net.atos.client.zgw.shared.util.Constants.APPLICATION_PROBLEM_JSON
 import static net.atos.client.zgw.shared.util.ZGWClientHeadersFactory.generateJWTToken;
 
 import java.net.URI;
+import java.security.NoSuchAlgorithmException;
 
+import javax.net.ssl.SSLContext;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.lang3.StringUtils;
 
 import net.atos.client.zgw.shared.util.LoggingFilter;
 import net.atos.client.zgw.zrc.ZRCClient;
@@ -31,12 +35,28 @@ public final class InvocationBuilderFactory {
 
     public static Invocation.Builder create(final URI uri) {
         if (client == null) {
-            client = ClientBuilder.newClient().register(LoggingFilter.class);
+            client = createClient();
         }
+
         return client.target(uri)
                 .request(MediaType.APPLICATION_JSON, APPLICATION_PROBLEM_JSON)
                 .header(HttpHeaders.AUTHORIZATION, generateJWTToken())
                 .header(ZRCClient.ACCEPT_CRS, ZRCClient.ACCEPT_CRS_VALUE)
                 .header(ZRCClient.CONTENT_CRS, ZRCClient.ACCEPT_CRS_VALUE);
+    }
+
+    private static Client createClient() {
+        final String proxyHost = System.getProperty("http.proxyHost");
+        final String proxyPort = System.getProperty("http.proxyPort");
+        try {
+            final ClientBuilder clientBuilder = ClientBuilder.newBuilder().sslContext(SSLContext.getDefault());
+            if (StringUtils.isNotEmpty(proxyHost) && StringUtils.isNumeric(proxyPort)) {
+                clientBuilder.property("org.jboss.resteasy.jaxrs.client.proxy.host", proxyHost)
+                        .property("org.jboss.resteasy.jaxrs.client.proxy.port", proxyPort);
+            }
+            return clientBuilder.build().register(LoggingFilter.class);
+        } catch (final NoSuchAlgorithmException e) {
+            throw new RuntimeException("Fout tijdens initialiseren van client", e);
+        }
     }
 }
