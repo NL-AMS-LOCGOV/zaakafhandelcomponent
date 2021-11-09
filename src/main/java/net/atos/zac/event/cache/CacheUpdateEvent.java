f402d344eb94bc46ee763bce836a19318feec316
Hans <hans.marks@atos.net>
2021-11-09 15:56:12 +0100

    ESUITEDEV-25869 Cache events vanuit notificaties

--- src/main/java/net/atos/zac/event/cache/CacheUpdateEvent.java
+++ src/main/java/net/atos/zac/event/cache/CacheUpdateEvent.java
@@ -5,12 +5,21 @@
 
 package net.atos.zac.event.cache;
 
+import static net.atos.zac.event.OpcodeEnum.UPDATED;
+
+import java.util.UUID;
+
+import javax.validation.constraints.NotNull;
+
 import net.atos.zac.event.AbstractUpdateEvent;
 
 // TODO Verplaatsen naar waar dit gebruikt worden .../event
-public class CacheUpdateEvent extends AbstractUpdateEvent<Object, Long> {
+public class CacheUpdateEvent extends AbstractUpdateEvent<CacheObjectTypeEnum, UUID> {
 
     private static final long serialVersionUID = -329301003012599689L;
+
+    @NotNull
+    private CacheObjectTypeEnum objectType;
 
     /**
      * Constructor for the sake of JAXB
@@ -19,8 +28,19 @@ public class CacheUpdateEvent extends AbstractUpdateEvent<Object, Long> {
         super();
     }
 
+    /**
+     * Constructor die alle verplichte velden bevat.
+     *
+     * @param objectType het type object waarop de operatie is uitgevoerd
+     * @param objectId   de identificatie van het object waarop een operatie is uitgevoerd
+     */
+    public CacheUpdateEvent(final CacheObjectTypeEnum objectType, final UUID objectId) {
+        super(UPDATED, objectId);
+        this.objectType = objectType;
+    }
+
     @Override
-    public Object getObjectType() {
-        return null;
+    public CacheObjectTypeEnum getObjectType() {
+        return objectType;
     }
 }
