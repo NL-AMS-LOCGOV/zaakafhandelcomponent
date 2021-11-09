225233900a3188e490d3a4ca214607e4fcb5bfee
Hans <hans.marks@atos.net>
2021-11-09 10:28:46 +0100

    ESUITEDEV-25868 Websocket events vanuit notificaties

--- src/main/java/net/atos/zac/websocket/event/ScreenObjectTypeEnum.java
+++ src/main/java/net/atos/zac/websocket/event/ScreenObjectTypeEnum.java
@@ -5,9 +5,9 @@
 
 package net.atos.zac.websocket.event;
 
-import static net.atos.zac.event.ActionEnum.CREATE;
-import static net.atos.zac.event.ActionEnum.DELETE;
-import static net.atos.zac.event.ActionEnum.UPDATE;
+import static net.atos.zac.event.OpcodeEnum.CREATE;
+import static net.atos.zac.event.OpcodeEnum.DELETE;
+import static net.atos.zac.event.OpcodeEnum.UPDATE;
 
 import java.net.URI;
 import java.util.UUID;
@@ -17,7 +17,7 @@ import org.flowable.task.api.TaskInfo;
 import net.atos.client.zgw.drc.model.EnkelvoudigInformatieobject;
 import net.atos.client.zgw.shared.util.URIUtil;
 import net.atos.client.zgw.zrc.model.Zaak;
-import net.atos.zac.event.ActionEnum;
+import net.atos.zac.event.OpcodeEnum;
 
 /**
  * Enumeratie die de soorten object wijzigingen bevat zoals die gebruikt worden door het {@link ScreenUpdateEvent}.
@@ -26,99 +26,99 @@ public enum ScreenObjectTypeEnum {
 
     ENKELVOUDIG_INFORMATIEOBJECT {
         @Override
-        protected ScreenUpdateEvent event(final ActionEnum action, final EnkelvoudigInformatieobject enkelvoudigInformatieobject) {
+        protected ScreenUpdateEvent event(final OpcodeEnum action, final EnkelvoudigInformatieobject enkelvoudigInformatieobject) {
             return instance(action, this, enkelvoudigInformatieobject);
         }
     },
 
     TAAK {
         @Override
-        protected ScreenUpdateEvent event(final ActionEnum action, final TaskInfo taak) {
+        protected ScreenUpdateEvent event(final OpcodeEnum action, final TaskInfo taak) {
             return instance(action, this, taak);
         }
     },
 
     ZAAK {
         @Override
-        protected ScreenUpdateEvent event(final ActionEnum action, final Zaak zaak) {
+        protected ScreenUpdateEvent event(final OpcodeEnum action, final Zaak zaak) {
             return instance(action, this, zaak);
         }
     },
 
     ZAAK_INFORMATIEOBJECT {
         @Override
-        protected ScreenUpdateEvent event(final ActionEnum action, final Zaak zaak) {
+        protected ScreenUpdateEvent event(final OpcodeEnum action, final Zaak zaak) {
             return instance(action, this, zaak);
         }
     },
 
     ZAAK_TAKEN {
         @Override
-        protected ScreenUpdateEvent event(final ActionEnum action, final Zaak zaak) {
+        protected ScreenUpdateEvent event(final OpcodeEnum action, final Zaak zaak) {
             return instance(action, this, zaak);
         }
     },
 
     ZAAK_BETROKKENEN {
         @Override
-        protected ScreenUpdateEvent event(final ActionEnum action, final Zaak zaak) {
+        protected ScreenUpdateEvent event(final OpcodeEnum action, final Zaak zaak) {
             return instance(action, this, zaak);
         }
     },
 
     ZAAK_ZAKEN {
         @Override
-        protected ScreenUpdateEvent event(final ActionEnum action, final Zaak zaak) {
+        protected ScreenUpdateEvent event(final OpcodeEnum action, final Zaak zaak) {
             return instance(action, this, zaak);
         }
     };
 
     // Dit is de uiteindelijke echte factory method
-    private static ScreenUpdateEvent instance(final ActionEnum action, final ScreenObjectTypeEnum type, final String id) {
+    private static ScreenUpdateEvent instance(final OpcodeEnum action, final ScreenObjectTypeEnum type, final String id) {
         return new ScreenUpdateEvent(action, type, id);
     }
 
     // Bij deze methods bepaal je zelf wat er als id gebruikt wordt, let er op dat dit consistent is met de andere methods
-    private static ScreenUpdateEvent instance(final ActionEnum action, final ScreenObjectTypeEnum type, final UUID uuid) {
+    private static ScreenUpdateEvent instance(final OpcodeEnum action, final ScreenObjectTypeEnum type, final UUID uuid) {
         return instance(action, type, uuid.toString());
     }
 
-    private static ScreenUpdateEvent instance(final ActionEnum action, final ScreenObjectTypeEnum type, final URI url) {
+    private static ScreenUpdateEvent instance(final OpcodeEnum action, final ScreenObjectTypeEnum type, final URI url) {
         return instance(action, type, URIUtil.parseUUIDFromResourceURI(url));
     }
 
     // Deze methods bepalen wat er als id gebruikt wordt, zodat dit overal hetzelfde is
-    private static ScreenUpdateEvent instance(final ActionEnum action, final ScreenObjectTypeEnum type, final Zaak zaak) {
+    private static ScreenUpdateEvent instance(final OpcodeEnum action, final ScreenObjectTypeEnum type, final Zaak zaak) {
         return instance(action, type, zaak.getUuid());
     }
 
-    private static ScreenUpdateEvent instance(final ActionEnum action, final ScreenObjectTypeEnum type,
+    private static ScreenUpdateEvent instance(final OpcodeEnum action, final ScreenObjectTypeEnum type,
             final EnkelvoudigInformatieobject enkelvoudigInformatieobject) {
         return instance(action, type, enkelvoudigInformatieobject.getUrl());
     }
 
-    private static ScreenUpdateEvent instance(final ActionEnum action, final ScreenObjectTypeEnum type, final TaskInfo taak) {
+    private static ScreenUpdateEvent instance(final OpcodeEnum action, final ScreenObjectTypeEnum type, final TaskInfo taak) {
         return instance(action, type, taak.getId());
     }
 
     // Deze methods bepalen op welke object types de verschillende argumenten zijn toegestaan
-    private ScreenUpdateEvent event(final ActionEnum action, final UUID uuid) {
+    private ScreenUpdateEvent event(final OpcodeEnum action, final UUID uuid) {
         return instance(action, this, uuid); // Toegestaan bij alle objecttypes
     }
 
-    private ScreenUpdateEvent event(final ActionEnum action, final URI url) {
+    private ScreenUpdateEvent event(final OpcodeEnum action, final URI url) {
         return instance(action, this, URIUtil.parseUUIDFromResourceURI(url)); // Toegestaan bij alle objecttypes
     }
 
-    protected ScreenUpdateEvent event(final ActionEnum action, final Zaak zaak) {
+    protected ScreenUpdateEvent event(final OpcodeEnum action, final Zaak zaak) {
         throw new IllegalArgumentException(); // Niet toegestaan behalve bij objecttypes waar deze method een override heeft
     }
 
-    protected ScreenUpdateEvent event(final ActionEnum action, final EnkelvoudigInformatieobject enkelvoudigInformatieobject) {
+    protected ScreenUpdateEvent event(final OpcodeEnum action, final EnkelvoudigInformatieobject enkelvoudigInformatieobject) {
         throw new IllegalArgumentException(); // Niet toegestaan behalve bij objecttypes waar deze method een override heeft
     }
 
-    protected ScreenUpdateEvent event(final ActionEnum action, final TaskInfo taak) {
+    protected ScreenUpdateEvent event(final OpcodeEnum action, final TaskInfo taak) {
         throw new IllegalArgumentException(); // Niet toegestaan behalve bij objecttypes waar deze method een override heeft
     }
 
--- src/test/java/net/atos/zac/SessionRegistryTest.java
+++ src/test/java/net/atos/zac/SessionRegistryTest.java
@@ -25,7 +25,7 @@ import javax.websocket.WebSocketContainer;
 
 import org.junit.jupiter.api.Test;
 
-import net.atos.zac.event.ActionEnum;
+import net.atos.zac.event.OpcodeEnum;
 import net.atos.zac.websocket.SessionRegistry;
 import net.atos.zac.websocket.event.ScreenObjectTypeEnum;
 import net.atos.zac.websocket.event.ScreenUpdateEvent;
@@ -328,17 +328,17 @@ public class SessionRegistryTest {
         }
     };
 
-    private static final ScreenUpdateEvent EVENT1 = new ScreenUpdateEvent(ActionEnum.CREATE, ScreenObjectTypeEnum.ZAAK, "1");
+    private static final ScreenUpdateEvent EVENT1 = new ScreenUpdateEvent(OpcodeEnum.CREATE, ScreenObjectTypeEnum.ZAAK, "1");
 
-    private static final ScreenUpdateEvent EVENT1a = new ScreenUpdateEvent(ActionEnum.CREATE, ScreenObjectTypeEnum.ZAAK, "\"1\"");
+    private static final ScreenUpdateEvent EVENT1a = new ScreenUpdateEvent(OpcodeEnum.CREATE, ScreenObjectTypeEnum.ZAAK, "\"1\"");
 
-    private static final ScreenUpdateEvent EVENT1b = new ScreenUpdateEvent(ActionEnum.CREATE, ScreenObjectTypeEnum.ZAAK, "\"\"1\"\"");
+    private static final ScreenUpdateEvent EVENT1b = new ScreenUpdateEvent(OpcodeEnum.CREATE, ScreenObjectTypeEnum.ZAAK, "\"\"1\"\"");
 
-    private static final ScreenUpdateEvent EVENT2 = new ScreenUpdateEvent(ActionEnum.CREATE, ScreenObjectTypeEnum.ZAAK, "2");
+    private static final ScreenUpdateEvent EVENT2 = new ScreenUpdateEvent(OpcodeEnum.CREATE, ScreenObjectTypeEnum.ZAAK, "2");
 
-    private static final ScreenUpdateEvent EVENT3 = new ScreenUpdateEvent(ActionEnum.CREATE, ScreenObjectTypeEnum.TAAK, "1");
+    private static final ScreenUpdateEvent EVENT3 = new ScreenUpdateEvent(OpcodeEnum.CREATE, ScreenObjectTypeEnum.TAAK, "1");
 
-    private static final ScreenUpdateEvent EVENT4 = new ScreenUpdateEvent(ActionEnum.UPDATE, ScreenObjectTypeEnum.ZAAK, "1");
+    private static final ScreenUpdateEvent EVENT4 = new ScreenUpdateEvent(OpcodeEnum.UPDATE, ScreenObjectTypeEnum.ZAAK, "1");
 
     @Test
     public void testFixGelijk() {
