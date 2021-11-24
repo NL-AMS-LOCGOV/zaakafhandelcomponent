455af69efd4bb48214eff1f4567503811e88a62a
Bart Lukens <bart.lukens@atos.net>
2021-11-24 11:56:28 +0100

    ESUITEDEV-25968 outside click

---
+++ src/main/app/src/app/shared/directives/outside-click.directive.spec.ts
@@ -0,0 +1,8 @@
+import {OutsideClickDirective} from './outside-click.directive';
+
+describe('OutsideClickDirective', () => {
+    it('should create an instance', () => {
+        const directive = new OutsideClickDirective();
+        expect(directive).toBeTruthy();
+    });
+});
---
+++ src/main/app/src/app/shared/directives/outside-click.directive.ts
@@ -0,0 +1,47 @@
+import {Directive, ElementRef, EventEmitter, Inject, OnDestroy, OnInit, Optional, Output, PLATFORM_ID} from '@angular/core';
+import {fromEvent, Subscription} from 'rxjs';
+import {DOCUMENT, isPlatformBrowser} from '@angular/common';
+import {filter} from 'rxjs/operators';
+
+@Directive({
+    selector: '[zacOutsideClick]'
+})
+export class OutsideClickDirective implements OnInit, OnDestroy {
+
+    @Output('zacOutsideClick') outsideClick = new EventEmitter<MouseEvent>();
+
+    private subscription: Subscription;
+
+    constructor(
+        private element: ElementRef,
+        @Optional() @Inject(DOCUMENT) private document: any,
+        @Inject(PLATFORM_ID) private platformId: Object) { }
+
+    ngOnInit() {
+        if (!isPlatformBrowser(this.platformId)) {
+            return;
+        }
+
+        setTimeout(() => {
+            this.subscription = fromEvent<MouseEvent>(this.document, 'click')
+            .pipe(
+                filter(event => {
+                    const clickTarget = event.target as HTMLElement;
+                    return !OutsideClickDirective.isOrContainsClickTarget(this.element.nativeElement, clickTarget);
+                })
+            )
+            .subscribe(event => this.outsideClick.emit(event));
+        }, 0);
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
+
+    private static isOrContainsClickTarget(element: HTMLElement, clickTarget: HTMLElement) {
+        // cdk-overlay-pane
+        return element == clickTarget || element.contains(clickTarget) || clickTarget.classList.contains('mat-option-text');
+    }
+}
