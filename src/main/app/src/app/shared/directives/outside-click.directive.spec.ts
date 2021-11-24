de905ff24bed1bfb230037fa4158b1663cb57a53
Bart Lukens <bart.lukens@atos.net>
2021-11-24 11:57:29 +0100

    ESUITEDEV-25968 outside click

--- src/main/app/src/app/shared/directives/outside-click.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import {OutsideClickDirective} from './outside-click.directive';
-
-describe('OutsideClickDirective', () => {
-    it('should create an instance', () => {
-        const directive = new OutsideClickDirective();
-        expect(directive).toBeTruthy();
-    });
-});
