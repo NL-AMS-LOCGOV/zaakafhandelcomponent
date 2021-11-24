828f17cd1a18b8130aaa228a553c3410423fa1c4
Hans <hans.marks@atos.net>
2021-11-24 14:43:57 +0100

    ESUITEDEV-25959 SessionRegistry upgrade

--- src/main/app/src/app/core/websocket/websocket.service.spec.ts
+++ src/main/app/src/app/core/websocket/websocket.service.spec.ts
@@ -9,8 +9,9 @@ import {ObjectType} from './model/object-type';
 import {WebsocketListener} from './model/websocket-listener';
 
 describe('WebsocketService', () => {
-    let service: WebsocketService;
-    let listeners: WebsocketListener[] = [];
+    var service: WebsocketService;
+    var listeners: WebsocketListener[] = [];
+    var received: number = 0;
 
     beforeEach(() => {
         // Gebruik de mock. N.B. daarmee kan ALLEEN de listeners-logica getest worden.
@@ -22,48 +23,66 @@ describe('WebsocketService', () => {
         expect(service).toBeTruthy();
     });
 
-    const EVENTS = 8192;
-    const OPCODES = [
-        Opcode.UPDATED,
-        Opcode.DELETED];
-    const OBJECT_TYPES = [
-        ObjectType.ENKELVOUDIG_INFORMATIEOBJECT,
-        ObjectType.TAAK,
-        ObjectType.ZAAK,
-        ObjectType.ZAAK_ROLLEN,
-        ObjectType.ZAAK_INFORMATIEOBJECTEN,
-        ObjectType.ZAAK_TAKEN];
-    const MAX_DELAY = 256; // ms
-
     it('should dispatch events to the correct listeners', (done) => {
+        const EVENTS = 10000;
+        reset();
         for (var i = 0; i < EVENTS; i++) {
-            var opcode: Opcode = OPCODES[Math.floor(Math.random() * OPCODES.length)];
-            var objectType: ObjectType = OBJECT_TYPES[Math.floor(Math.random() * OBJECT_TYPES.length)];
-            var delay: string = Math.floor(Math.random() * MAX_DELAY).toString();
-            listeners.push(service.addListener(opcode, objectType, delay, callback(opcode, objectType, delay, done)));
+            listeners.push(addRandomListener(EVENTS, done));
         }
     });
 
-    var received: number = 0;
+    it('should not dispatch events to suspended listeners', (done) => {
+        reset();
+        // TODO ESUITEDEV-25959
+        listeners.push(addRandomListener(1, done));
+    });
 
-    function callback(opcode: Opcode, objectType: ObjectType, objectId: string, done) {
+    it('should not dispatch events to removed listeners', (done) => {
+        reset();
+        // TODO ESUITEDEV-25959
+        listeners.push(addRandomListener(1, done));
+    });
+
+    // Does not test ANY opcode and/or ANY objectType because the mock doesn't support it (see SessionRegistryTest for those)
+    function addRandomListener(expected: number, done): WebsocketListener {
+        const OPCODES = [
+            Opcode.UPDATED,
+            Opcode.DELETED];
+        const OBJECT_TYPES = [
+            ObjectType.ENKELVOUDIG_INFORMATIEOBJECT,
+            ObjectType.TAAK,
+            ObjectType.ZAAK,
+            ObjectType.ZAAK_ROLLEN,
+            ObjectType.ZAAK_INFORMATIEOBJECTEN,
+            ObjectType.ZAAK_TAKEN];
+        const MAX_DELAY = 512; // ms
+
+        var opcode: Opcode = OPCODES[Math.floor(Math.random() * OPCODES.length)];
+        var objectType: ObjectType = OBJECT_TYPES[Math.floor(Math.random() * OBJECT_TYPES.length)];
+        var delay: string = Math.floor(Math.random() * MAX_DELAY).toString();
+        return service.addListener(opcode, objectType, delay, callback(opcode, objectType, delay, expected, done));
+    }
+
+    function callback(opcode: Opcode, objectType: ObjectType, objectId: string, expected: number, done) {
         return event => {
             expect(typeof event.timestamp).toEqual('number');
             expect(event.opcode).toEqual(opcode);
             expect(event.objectType).toEqual(objectType);
             expect(event.objectId).toEqual(objectId);
             expect(event.key).toEqual(opcode + ';' + objectType + ';' + objectId);
-            if (++received == EVENTS) {
+            if (++received == expected) {
                 done();
-                cleanup();
+                reset();
             }
         };
     }
 
-    function cleanup() {
+    // This depends on listeners (do make sure it to keep it consistent with what is registered with the service)
+    function reset() {
         for (var i = 0; i < listeners.length; i++) {
             service.removeListener(listeners[i]);
         }
         listeners.length = 0;
+        received = 0;
     }
 });
