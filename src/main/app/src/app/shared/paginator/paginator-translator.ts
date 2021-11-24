8c5480ad0589a06b7ddfc0f599fdc1ee492cdf42
Edwin Lambregts <edwin.lambregts@atos.net>
2021-11-24 16:22:58 +0100

    ESUITEDEV-25947 Vertaling toevoegen

--- src/main/app/src/app/shared/paginator/paginator-translator.ts
+++ src/main/app/src/app/shared/paginator/paginator-translator.ts
@@ -13,21 +13,21 @@ export class PaginatorTranslator {
     getTranslatedPaginator() {
         const paginator = new MatPaginatorIntl();
 
-        paginator.itemsPerPageLabel = this.translateService.instant('paginator.items.per.pagina');
-        paginator.nextPageLabel = this.translateService.instant('paginator.volgende');
-        paginator.previousPageLabel = this.translateService.instant('paginator.vorige');
+        paginator.itemsPerPageLabel = this.translateService.instant('msg.pagina.items');
+        paginator.nextPageLabel = this.translateService.instant('actie.pagina.volgende');
+        paginator.previousPageLabel = this.translateService.instant('actie.pagina.vorige');
         paginator.getRangeLabel = this.translatedRangeLabel;
 
         return paginator;
     }
 
     private translatedRangeLabel = (page: number, pageSize: number, length: number) => {
-        if (length == 0 || pageSize == 0) { return `0 ${this.translateService.instant('paginator.van')} ${length}`; }
+        if (length == 0 || pageSize == 0) { return `0 ${this.translateService.instant('msg.pagina', length)}`; }
         length = Math.max(length, 0);
         const startIndex = page * pageSize;
         const endIndex = startIndex < length ?
             Math.min(startIndex + pageSize, length) :
             startIndex + pageSize;
-        return `${startIndex + 1} - ${endIndex} ${this.translateService.instant('paginator.van')} ${length}`;
+        return `${startIndex + 1} - ${endIndex} ${this.translateService.instant('msg.pagina')} ${length}`;
     };
 }
