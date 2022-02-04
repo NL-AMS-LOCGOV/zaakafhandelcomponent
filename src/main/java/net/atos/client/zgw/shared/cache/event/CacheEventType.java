a6eb15db3800ec7af4422b9f80c5a046923f7867
Hans <hans.marks@atos.net>
2022-02-04 16:02:02 +0100

    zaakafhandelcomponent-397 Caching events ook direct (niet via notificaties) versturen

--- src/main/java/net/atos/client/zgw/shared/cache/event/CacheEventType.java
+++ src/main/java/net/atos/client/zgw/shared/cache/event/CacheEventType.java
@@ -7,35 +7,71 @@ package net.atos.client.zgw.shared.cache.event;
 
 
 import java.net.URI;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.logging.Logger;
 
+import net.atos.client.zgw.zrc.model.Rol;
+import net.atos.client.zgw.zrc.model.Status;
+import net.atos.client.zgw.ztc.model.Zaaktype;
 import net.atos.zac.notificaties.Channel;
 import net.atos.zac.notificaties.Notificatie;
 
 /**
- * Enumeratie die de soorten events (objecttypen) bevat zoals die gebruikt worden door het {@link CacheEvent}.
+ * Enumeration of the type of objects that can be referenced by a {@link CacheEvent} event.
  */
 public enum CacheEventType {
-    ZAAKROL,
-    ZAAKSTATUS,
-    ZAAKTYPE;
+    ZAAKROL {
+        @Override
+        public final CacheEvent event(final Rol<?> rol) {
+            return instance(this, rol);
+        }
+    },
+    ZAAKSTATUS {
+        @Override
+        public final CacheEvent event(final Status status) {
+            return instance(this, status);
+        }
+    },
+    ZAAKTYPE {
+        @Override
+        public final CacheEvent event(final Zaaktype zaaktype) {
+            return instance(this, zaaktype);
+        }
+    };
 
-    private static final Logger LOG = Logger.getLogger(CacheEventType.class.getName());
-
-    // Dit is de uiteindelijke echte factory method
+    // This is the factory method
     private static CacheEvent instance(final CacheEventType objectType, final URI url) {
         return new CacheEvent(objectType, url);
     }
 
-    private CacheEvent event(final Notificatie.ResourceInfo resource) {
-        return instance(this, resource.getUrl());
+    // These methods determine what is used as an id, so that it is the same everywhere
+    private static CacheEvent instance(final CacheEventType objectType, final Rol<?> rol) {
+        return instance(objectType, rol.getZaak());
     }
 
-    public final CacheEvent changed(final URI url) {
-        return instance(this, url);
+    private static CacheEvent instance(final CacheEventType objectType, final Status status) {
+        return instance(objectType, status.getUrl());
+    }
+
+    private static CacheEvent instance(final CacheEventType objectType, final Zaaktype zaaktype) {
+        return instance(objectType, zaaktype.getUrl());
+    }
+
+    // These methods determine on which object types the different arguments are allowed
+    private CacheEvent event(final Notificatie.ResourceInfo resource) {
+        return instance(this, resource.getUrl()); // Allowed with all object types
+    }
+
+    public CacheEvent event(final Rol<?> rol) {
+        throw new IllegalArgumentException(); // Not allowed except for object types where this method has an override
+    }
+
+    public CacheEvent event(final Status status) {
+        throw new IllegalArgumentException(); // Not allowed except for object types where this method has an override
+    }
+
+    public CacheEvent event(final Zaaktype zaaktype) {
+        throw new IllegalArgumentException(); // Not allowed except for object types where this method has an override
     }
 
     /**
@@ -50,70 +86,50 @@ public enum CacheEventType {
      * @return the set of events that the parameters map to
      */
     public static Set<CacheEvent> getEvents(final Channel channel, final Notificatie.ResourceInfo mainResource, final Notificatie.ResourceInfo resource) {
+        final Set<CacheEvent> events = new HashSet<>();
         switch (channel) {
             case ZAKEN:
-                return getChannelZakenEvents(mainResource, resource);
+                switch (resource.getType()) {
+                    case ROL:
+                        switch (resource.getAction()) {
+                            case CREATE:
+                            case UPDATE:
+                            case DELETE:
+                                events.add(CacheEventType.ZAAKROL.event(mainResource));
+                                break;
+                            default:
+                                break;
+                        }
+                    case STATUS:
+                        switch (resource.getAction()) {
+                            case UPDATE:
+                            case DELETE:
+                                events.add(CacheEventType.ZAAKSTATUS.event(resource));
+                                break;
+                            default:
+                                break;
+                        }
+                    default:
+                        break;
+                }
+                break;
             case ZAAKTYPEN:
-                return getChannelZaaktypenEvents(resource);
+                switch (resource.getType()) {
+                    case ZAAKTYPE:
+                        switch (resource.getAction()) {
+                            case UPDATE:
+                            case DELETE:
+                                events.add(CacheEventType.ZAAKTYPE.event(resource));
+                                break;
+                            default:
+                                break;
+                        }
+                    default:
+                        break;
+                }
             default:
-                return Collections.emptySet();
+                break;
         }
-    }
-
-    private static Set<CacheEvent> getChannelZakenEvents(final Notificatie.ResourceInfo mainResource, final Notificatie.ResourceInfo resource) {
-        switch (resource.getType()) {
-            case ROL:
-                return getChannelZakenResourceRolEvents(mainResource, resource);
-            case STATUS:
-                return getChannelZakenResourceStatusEvents(resource);
-            default:
-                return Collections.emptySet();
-        }
-    }
-
-    private static Set<CacheEvent> getChannelZakenResourceRolEvents(final Notificatie.ResourceInfo mainResource, final Notificatie.ResourceInfo resource) {
-        switch (resource.getAction()) {
-            case CREATE:
-            case UPDATE:
-            case DELETE:
-                final Set<CacheEvent> events = new HashSet<>();
-                events.add(CacheEventType.ZAAKROL.event(mainResource));
-                return events;
-            default:
-                return Collections.emptySet();
-        }
-    }
-
-    private static Set<CacheEvent> getChannelZakenResourceStatusEvents(final Notificatie.ResourceInfo resource) {
-        switch (resource.getAction()) {
-            case UPDATE:
-            case DELETE:
-                final Set<CacheEvent> events = new HashSet<>();
-                events.add(CacheEventType.ZAAKSTATUS.event(resource));
-                return events;
-            default:
-                return Collections.emptySet();
-        }
-    }
-
-    private static Set<CacheEvent> getChannelZaaktypenEvents(final Notificatie.ResourceInfo resource) {
-        switch (resource.getType()) {
-            case ZAAKTYPE:
-                return getChannelZaaktypenResourceZaaktypeEvents(resource);
-            default:
-                return Collections.emptySet();
-        }
-    }
-
-    private static Set<CacheEvent> getChannelZaaktypenResourceZaaktypeEvents(final Notificatie.ResourceInfo resource) {
-        switch (resource.getAction()) {
-            case UPDATE:
-            case DELETE:
-                final Set<CacheEvent> events = new HashSet<>();
-                events.add(CacheEventType.ZAAKTYPE.event(resource));
-                return events;
-            default:
-                return Collections.emptySet();
-        }
+        return events;
     }
 }
