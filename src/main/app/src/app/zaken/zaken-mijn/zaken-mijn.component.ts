f6e7e2c8db354b64a14eab4f0aaa12ec37ecada5
Edwin Lambregts <edwin.lambregts@atos.net>
2022-02-18 15:30:09 +0100

    zaakafhandelcomponent-409 Knop om filters en sorteringen te wissen

--- src/main/app/src/app/zaken/zaken-mijn/zaken-mijn.component.ts
+++ src/main/app/src/app/zaken/zaken-mijn/zaken-mijn.component.ts
@@ -129,14 +129,12 @@ export class ZakenMijnComponent implements OnInit, AfterViewInit, OnDestroy {
     }
 
     resetSearchCriteria() {
-        if (this.werklijstData) {
-            this.dataSource.filters = {};
-            this.dataSource.initColumns(this.initialColumns());
-            this.paginator.pageIndex = 0;
-            this.paginator.pageSize = 25;
-            this.sort.active = '';
-            this.sort.direction = '';
-        }
+        this.dataSource.filters = {};
+        this.dataSource.initColumns(this.initialColumns());
+        this.paginator.pageIndex = 0;
+        this.paginator.pageSize = 25;
+        this.sort.active = '';
+        this.sort.direction = '';
     }
 
 }
