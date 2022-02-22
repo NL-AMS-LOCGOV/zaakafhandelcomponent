263cc8b79f02659d8e77b1feb5e38e49d8592320
Marco Jacobs <marco.jacobs@atos.net>
2022-02-22 15:51:01 +0100

    zaakafhandelcomponent-287

--- src/main/app/src/app/shared/edit/edit-datum-groep/edit-datum-groep.component.ts
+++ src/main/app/src/app/shared/edit/edit-datum-groep/edit-datum-groep.component.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewEncapsulation} from '@angular/core';
 import {EditComponent} from '../edit.component';
 import {MaterialFormBuilderService} from '../../material-form-builder/material-form-builder.service';
 import {DateFormField} from '../../material-form-builder/form-components/date/date-form-field';
@@ -17,6 +17,7 @@ import * as moment from 'moment/moment';
 @Component({
     selector: 'zac-edit-datum-groep',
     templateUrl: './edit-datum-groep.component.html',
+    encapsulation: ViewEncapsulation.None,
     styleUrls: ['../../static-text/static-text.component.less', '../edit.component.less', './edit-datum-groep.component.less']
 })
 export class EditDatumGroepComponent extends EditComponent {
