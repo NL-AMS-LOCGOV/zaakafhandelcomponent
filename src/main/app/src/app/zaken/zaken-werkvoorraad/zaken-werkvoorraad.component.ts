56ed4705ca422f7849df78687fe9ea3b80630175
Edwin Lambregts <edwin.lambregts@atos.net>
2022-02-23 15:04:49 +0100

    zaakafhandelcomponent-534 BUG: Na herladen page, wordt de andere

--- src/main/app/src/app/zaken/zaken-werkvoorraad/zaken-werkvoorraad.component.ts
+++ src/main/app/src/app/zaken/zaken-werkvoorraad/zaken-werkvoorraad.component.ts
@@ -92,21 +92,7 @@ export class ZakenWerkvoorraadComponent implements AfterViewInit, OnInit, OnDest
     }
 
     ngOnDestroy() {
-        const flatListColumns = JSON.stringify([...this.dataSource.columns]);
-        const werklijstData = new WerklijstData();
-        werklijstData.searchParameters = this.dataSource.zoekParameters;
-        werklijstData.filters = this.dataSource.filters;
-        werklijstData.columns = flatListColumns;
-        werklijstData.sorting = {
-            column: this.sort.active,
-            direction: this.sort.direction
-        };
-        werklijstData.paginator = {
-            page: this.paginator.pageIndex,
-            pageSize: this.paginator.pageSize
-        };
-
-        this.sessionStorageService.setSessionStorage('zakenWerkvoorraadData', werklijstData);
+        this.saveSearchQuery();
     }
 
     private zaaktypesOphalen() {
@@ -266,6 +252,24 @@ export class ZakenWerkvoorraadComponent implements AfterViewInit, OnInit, OnDest
         return Conditionals.isOverschreden(datum);
     }
 
+    saveSearchQuery() {
+        const flatListColumns = JSON.stringify([...this.dataSource.columns]);
+        const werklijstData = new WerklijstData();
+        werklijstData.searchParameters = this.dataSource.zoekParameters;
+        werklijstData.filters = this.dataSource.filters;
+        werklijstData.columns = flatListColumns;
+        werklijstData.sorting = {
+            column: this.sort.active,
+            direction: this.sort.direction
+        };
+        werklijstData.paginator = {
+            page: this.paginator.pageIndex,
+            pageSize: this.paginator.pageSize
+        };
+
+        this.sessionStorageService.setSessionStorage('zakenWerkvoorraadData', werklijstData);
+    }
+
     resetSearchCriteria() {
         this.dataSource.zoekParameters = {
             selectie: 'groep',
@@ -278,5 +282,7 @@ export class ZakenWerkvoorraadComponent implements AfterViewInit, OnInit, OnDest
         this.paginator.pageSize = 25;
         this.sort.active = '';
         this.sort.direction = '';
+
+        this.saveSearchQuery();
     }
 }
