ca47422ee8a2cf5e0a41f6751c4343120b4085c9
Andy Verberne <andy.verberne@atos.net>
2022-03-03 08:12:26 +0100

    zaakafhandelcomponent-541 Kleine verbetering in ophalen werkvoorraad

--- src/main/java/net/atos/zac/mail/MailService.java
+++ src/main/java/net/atos/zac/mail/MailService.java
@@ -1,5 +1,11 @@
 package net.atos.zac.mail;
 
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import com.mailjet.client.ClientOptions;
 import com.mailjet.client.MailjetClient;
 import com.mailjet.client.MailjetRequest;
@@ -7,22 +13,17 @@ import com.mailjet.client.MailjetResponse;
 import com.mailjet.client.errors.MailjetException;
 import com.mailjet.client.resource.Emailv31;
 
-import org.eclipse.microprofile.config.ConfigProvider;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import javax.enterprise.context.ApplicationScoped;
-
 @ApplicationScoped
 public class MailService {
 
     private static final String MAILJET_API_KEY = ConfigProvider.getConfig().getValue("mailjet.api.key", String.class);
-    private static final String MAILJET_API_SECRET_KEY = ConfigProvider.getConfig().getValue("mailjet.api.secret.key",
-                                                                                       String.class);
+
+    private static final String MAILJET_API_SECRET_KEY = ConfigProvider.getConfig().getValue("mailjet.api.secret.key", String.class);
+
     private static final String MAIL_DOMEIN = ConfigProvider.getConfig().getValue("mail.domein", String.class);
 
-    private final ClientOptions clientOptions = ClientOptions.builder().apiKey(MAILJET_API_KEY)
-            .apiSecretKey(MAILJET_API_SECRET_KEY).build();
+    private final ClientOptions clientOptions = ClientOptions.builder().apiKey(MAILJET_API_KEY).apiSecretKey(MAILJET_API_SECRET_KEY).build();
+
     private final MailjetClient mailjetClient = new MailjetClient(clientOptions);
 
     public MailjetResponse sendMail(final String ontvanger, final String onderwerp, final String body) throws MailjetException {
