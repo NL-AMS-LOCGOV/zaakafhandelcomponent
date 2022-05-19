6f5d4c6226cf0c3395b93ff97b7d7f7d673d80ae
Marco Jacobs <marco.jacobs@atos.net>
2022-05-19 08:57:48 +0200

    zaakafhandelcomponent-897 - reviewcommentaar

--- src/main/app/src/app/informatie-objecten/informatie-object-edit/informatie-object-edit.component.ts
+++ src/main/app/src/app/informatie-objecten/informatie-object-edit/informatie-object-edit.component.ts
@@ -88,23 +88,15 @@ export class InformatieObjectEditComponent implements OnInit {
                                                  .validators(Validators.required)
                                                  .build();
 
-        let status;
-        if (this.infoObject.status) {
-            status = new SelectFormFieldBuilder().id('status').label('status')
-                                                       .validators(Validators.required)
-                                                       .optionLabel('label').options(informatieobjectStatussen)
-                                                       .value({
-                                                           label: this.translateService.instant(
-                                                               'informatieobject.status.' + this.infoObject.status.toUpperCase()),
-                                                           value: this.infoObject.status.toUpperCase()
-                                                       })
-                                                       .build();
-        } else {
-            status = new SelectFormFieldBuilder().id('status').label('status')
+        const status = new SelectFormFieldBuilder().id('status').label('status')
                                                  .validators(Validators.required)
                                                  .optionLabel('label').options(informatieobjectStatussen)
+                                                 .value(this.infoObject.status ? {
+                                                     label: this.translateService.instant(
+                                                         'informatieobject.status.' + this.infoObject.status.toUpperCase()),
+                                                     value: this.infoObject.status.toUpperCase()
+                                                 } : null)
                                                  .build();
-        }
 
         const verzenddatum = new DateFormFieldBuilder().id('verzenddatum')
                                                        .label('verzenddatum')
