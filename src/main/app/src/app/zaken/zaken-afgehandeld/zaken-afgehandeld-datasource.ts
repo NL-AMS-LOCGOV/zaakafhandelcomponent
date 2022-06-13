7a78908963b3dd02e8c91b1fbfd205cffcddb463
Ronnie Koopmans <ronnie.koopmans@atos.net>
2022-06-13 14:41:12 +0200

    zaakafhandelcomponent-1100 afgehandelde zaken omzetten

--- src/main/app/src/app/zaken/zaken-afgehandeld/zaken-afgehandeld-datasource.ts
+++ src/main/app/src/app/zaken/zaken-afgehandeld/zaken-afgehandeld-datasource.ts
@@ -3,56 +3,19 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-import {finalize} from 'rxjs/operators';
 import {UtilService} from '../../core/service/util.service';
 import {ZoekenTableDataSource} from '../../shared/dynamic-table/datasource/zoeken-table-data-source';
 import {ZaakZoekObject} from '../../zoeken/model/zaken/zaak-zoek-object';
 import {ZoekenService} from '../../zoeken/zoeken.service';
 import {ZoekParameters} from '../../zoeken/model/zoek-parameters';
-import {ZoekResultaat} from '../../zoeken/model/zoek-resultaat';
-import {SessionStorageUtil} from '../../shared/storage/session-storage.util';
-
-import {ColumnPickerValue} from '../../shared/dynamic-table/column-picker/column-picker-value';
 
 export class ZakenAfgehandeldDatasource extends ZoekenTableDataSource<ZaakZoekObject> {
 
-    zoekParameters: ZoekParameters = SessionStorageUtil.getItem('zakenMijnZoekparameters', new ZoekParameters());
-
-    constructor(private zoekenService: ZoekenService, private utilService: UtilService) {
-        super();
+    constructor(zoekenService: ZoekenService, utilService: UtilService) {
+        super('zakenAfgehandeld', zoekenService, utilService);
     }
 
-    getZoekParameters(): ZoekParameters {
-        this.zoekParameters.alleenAfgeslotenZaken = true;
-        this.zoekParameters.page = this.paginator.pageIndex;
-        this.zoekParameters.rows = this.paginator.pageSize;
-        this.zoekParameters.sorteerRichting = this.sort.direction;
-        this.zoekParameters.sorteerVeld = this.sort.active;
-        SessionStorageUtil.setItem('zakenAfgehandeldZoekparameters', this.zoekParameters);
-        return this.zoekParameters;
-    }
-
-    load(): void {
-        this.utilService.setLoading(true);
-        this.zoekenService.list(this.getZoekParameters())
-            .pipe(
-                finalize(() => this.utilService.setLoading(false))
-            ).subscribe(zaakResponse => {
-                this.setData(zaakResponse as ZoekResultaat<ZaakZoekObject>);
-            }
-        );
-    }
-
-    reset() {
-        this.zoekParameters = new ZoekParameters();
-        this.sort.active = this.zoekParameters.sorteerVeld;
-        this.sort.direction = this.zoekParameters.sorteerRichting;
-        this.paginator.pageIndex = 0;
-        this.paginator.pageSize = this.zoekParameters.rows;
-        this.load();
-    }
-
-    initColumns(defaultColumns: Map<string, ColumnPickerValue>): void {
-        this._initColumns('zakenAfgehandeldColumns', defaultColumns);
+    protected initZoekparameters(zoekParameters: ZoekParameters) {
+        zoekParameters.alleenAfgeslotenZaken = true;
     }
 }
