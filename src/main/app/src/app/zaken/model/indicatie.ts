6e5e0a2d4d73a489884d4be144be13a6e358e95e
Ronnie Koopmans <ronnie.koopmans@atos.net>
2022-06-13 15:30:55 +0200

    zaakafhandelcomponent-888 Tonen van indicaties

--- src/main/app/src/app/zaken/model/indicatie.ts
+++ src/main/app/src/app/zaken/model/indicatie.ts
@@ -3,14 +3,16 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
+import {ThemePalette} from '@angular/material/core';
+
 export class Indicatie {
-    constructor(indicatie: string, toelichting: string, color: string = 'accent') {
+    constructor(indicatie: string, toelichting: string, color: ThemePalette = 'accent') {
         this.color = color;
         this.naam = indicatie;
         this.info = toelichting;
     }
 
-    color: string;
+    color: ThemePalette;
     naam: string;
     info: string;
 }
