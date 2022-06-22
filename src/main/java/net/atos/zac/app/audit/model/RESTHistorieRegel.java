cf1b7f36d7331fdf1198a4210588b32ac3144799
Andy Verberne <andy.verberne@atos.net>
2022-06-22 15:22:17 +0200

    zaakafhandelcomponent-1021 ACCEPTATIE BUG: bij Historie 01-01-3428 als je document toevoegt met 4 cijferig nummer "3428".

--- src/main/java/net/atos/zac/app/audit/model/RESTHistorieRegel.java
+++ src/main/java/net/atos/zac/app/audit/model/RESTHistorieRegel.java
@@ -5,23 +5,30 @@
 
 package net.atos.zac.app.audit.model;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.apache.commons.lang3.BooleanUtils;
+
+import net.atos.client.zgw.shared.model.AbstractEnum;
 
 public class RESTHistorieRegel {
 
-    public RESTHistorieRegel(final String attribuutLabel) {
-        this.attribuutLabel = attribuutLabel;
-    }
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
-    public RESTHistorieRegel(final String attribuutLabel, final Object oudeWaarde, final Object nieuweWaarde) {
-        this.attribuutLabel = attribuutLabel;
-        if (oudeWaarde != null) {
-            this.oudeWaarde = oudeWaarde.toString();
-        }
-        if (nieuweWaarde != null) {
-            this.nieuweWaarde = nieuweWaarde.toString();
-        }
-    }
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm").withZone(ZoneId.systemDefault());
+
+    private static final String TRUE = "Ja";
+
+    private static final String FALSE = "Nee";
+
+    public final String attribuutLabel;
+
+    public final String oudeWaarde;
+
+    public final String nieuweWaarde;
 
     public ZonedDateTime datumTijd;
 
@@ -29,11 +36,51 @@ public class RESTHistorieRegel {
 
     public String applicatie;
 
-    public String attribuutLabel;
-
-    public String oudeWaarde;
-
-    public String nieuweWaarde;
-
     public String toelichting;
+
+    public RESTHistorieRegel(final String attribuutLabel, final String oudeWaarde, final String nieuweWaarde) {
+        this.attribuutLabel = attribuutLabel;
+        this.oudeWaarde = oudeWaarde;
+        this.nieuweWaarde = nieuweWaarde;
+    }
+
+    public RESTHistorieRegel(final String attribuutLabel, final LocalDate oudeWaarde, final LocalDate nieuweWaarde) {
+        this.attribuutLabel = attribuutLabel;
+        this.oudeWaarde = toWaarde(oudeWaarde);
+        this.nieuweWaarde = toWaarde(nieuweWaarde);
+    }
+
+    public RESTHistorieRegel(final String attribuutLabel, final Boolean oudeWaarde, final Boolean nieuweWaarde) {
+        this.attribuutLabel = attribuutLabel;
+        this.oudeWaarde = toWaarde(oudeWaarde);
+        this.nieuweWaarde = toWaarde(nieuweWaarde);
+    }
+
+    public RESTHistorieRegel(final String attribuutLabel, final AbstractEnum<?> oudeWaarde, final AbstractEnum<?> nieuweWaarde) {
+        this.attribuutLabel = attribuutLabel;
+        this.oudeWaarde = toWaarde(oudeWaarde);
+        this.nieuweWaarde = toWaarde(nieuweWaarde);
+    }
+
+    public RESTHistorieRegel(final String attribuutLabel, final ZonedDateTime oudeWaarde, final ZonedDateTime nieuweWaarde) {
+        this.attribuutLabel = attribuutLabel;
+        this.oudeWaarde = toWaarde(oudeWaarde);
+        this.nieuweWaarde = toWaarde(nieuweWaarde);
+    }
+
+    private String toWaarde(final LocalDate date) {
+        return date != null ? DATE_FORMATTER.format(date) : null;
+    }
+
+    private String toWaarde(final ZonedDateTime date) {
+        return date != null ? DATE_TIME_FORMATTER.format(date) : null;
+    }
+
+    private String toWaarde(final AbstractEnum<?> abstractEnum) {
+        return abstractEnum != null ? abstractEnum.toValue() : null;
+    }
+
+    private String toWaarde(final Boolean bool) {
+        return bool != null ? BooleanUtils.toString(bool, TRUE, FALSE) : null;
+    }
 }
