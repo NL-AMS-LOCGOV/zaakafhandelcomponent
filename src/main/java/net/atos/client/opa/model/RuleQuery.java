1b03b420712ebd321557ead0c39c137ca3dd0a17
Andy Verberne <andy.verberne@atos.net>
2022-07-03 12:23:31 +0200

    zaakafhandelcomponent-1145 Geautomatiseerd deployen van SOLr schema

--- src/main/java/net/atos/client/opa/model/RuleQuery.java
+++ src/main/java/net/atos/client/opa/model/RuleQuery.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-package net.atos.zac.policy.model;
+package net.atos.client.opa.model;
 
 public class RuleQuery<T> {
 
--- src/main/java/net/atos/zac/policy/input/ZaakInput.java
+++ src/main/java/net/atos/zac/policy/input/ZaakInput.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-package net.atos.zac.policy.model.input;
+package net.atos.zac.policy.input;
 
 import net.atos.zac.authentication.LoggedInUser;
 
