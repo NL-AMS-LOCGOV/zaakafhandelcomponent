8ca88e91bb416425d932e2565332ab794383f93b
Marco Jacobs <marco.jacobs@atos.net>
2022-08-30 13:56:48 +0200

    zaakafhandelcomponent-443 - Werklijsten exporteren naar CSV

---
+++ src/main/app/src/app/csv/csv.service.ts
@@ -0,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Atos
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
+import {ZoekParameters} from '../zoeken/model/zoek-parameters';
+import {catchError} from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {FoutAfhandelingService} from '../fout-afhandeling/fout-afhandeling.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CsvService {
+
+    constructor(private http: HttpClient, private foutAfhandelingService: FoutAfhandelingService) {
+    }
+
+    private basepath = '/rest/csv';
+
+    exportToCSV(zoekParameters: ZoekParameters) {
+        return this.http.post(`${this.basepath}/export`, zoekParameters, {responseType: 'blob'}).pipe(
+            catchError(err => this.foutAfhandelingService.foutAfhandelen(err))
+        );
+    }
+}
