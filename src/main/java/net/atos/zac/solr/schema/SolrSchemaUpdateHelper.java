ddce83dc1446a8f0c616870549715e4d44c26053
Andy Verberne <andy.verberne@atos.net>
2022-11-24 17:23:13 +0100

    zaakafhandelcomponent-1823 SOLR schema automatisch deployen

--- src/main/java/net/atos/zac/solr/schema/SolrSchemaUpdateHelper.java
+++ src/main/java/net/atos/zac/solr/schema/SolrSchemaUpdateHelper.java
@@ -5,6 +5,7 @@
 
 package net.atos.zac.solr.schema;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.solr.client.solrj.request.schema.SchemaRequest;
@@ -21,7 +22,40 @@ public final class SolrSchemaUpdateHelper {
 
     private static final String STORED = "stored";
 
+    private static final String DEFAULT = "default";
+
+    private static final String DOC_VALUES = "docValues";
+
+    private static final String MULTI_VALUED = "multiValued";
+
+    private static final String SOURCE = "source";
+
+    private static final String DEST = "dest";
+
     private SolrSchemaUpdateHelper() {
+    }
+
+    public static SchemaRequest.AddField addField(final String name, final String type) {
+        return new SchemaRequest.AddField(
+                Map.of(NAME, name,
+                       TYPE, type
+                ));
+    }
+
+    public static SchemaRequest.AddField addField(final String name, final String type, final String _default) {
+        return new SchemaRequest.AddField(
+                Map.of(NAME, name,
+                       TYPE, type,
+                       DEFAULT, _default
+                ));
+    }
+
+    public static SchemaRequest.AddField addField(final String name, final String type, final boolean docValues) {
+        return new SchemaRequest.AddField(
+                Map.of(NAME, name,
+                       TYPE, type,
+                       DOC_VALUES, docValues
+                ));
     }
 
     public static SchemaRequest.AddField addField(final String name, final String type, final boolean indexed,
@@ -34,11 +68,74 @@ public final class SolrSchemaUpdateHelper {
                 ));
     }
 
+    public static SchemaRequest.AddField addField(final String name, final String type, final boolean indexed,
+            final boolean stored, final boolean docValues) {
+        return new SchemaRequest.AddField(
+                Map.of(NAME, name,
+                       TYPE, type,
+                       INDEXED, indexed,
+                       STORED, stored,
+                       DOC_VALUES, docValues
+                ));
+    }
+
+    public static SchemaRequest.AddField addFieldMultiValued(final String name, final String type) {
+        return new SchemaRequest.AddField(
+                Map.of(NAME, name,
+                       TYPE, type,
+                       MULTI_VALUED, true
+                ));
+    }
+
+    public static SchemaRequest.AddField addFieldMultiValued(final String name, final String type,
+            final boolean docValues) {
+        return new SchemaRequest.AddField(
+                Map.of(NAME, name,
+                       TYPE, type,
+                       DOC_VALUES, docValues,
+                       MULTI_VALUED, true
+                ));
+    }
+
+    public static SchemaRequest.AddField addFieldMultiValued(final String name, final String type,
+            final boolean indexed, final boolean stored) {
+        return new SchemaRequest.AddField(
+                Map.of(NAME, name,
+                       TYPE, type,
+                       INDEXED, indexed,
+                       STORED, stored,
+                       MULTI_VALUED, true
+                ));
+    }
+
+    public static SchemaRequest.AddCopyField addCopyField(final String source, final String dest) {
+        return new SchemaRequest.AddCopyField(source, List.of(dest));
+    }
+
+    public static SchemaRequest.AddCopyField addCopyField(final String source, final String dest1, final String dest2) {
+        return new SchemaRequest.AddCopyField(source, List.of(dest1, dest2));
+    }
+
+    public static SchemaRequest.AddCopyField addCopyField(final String source, final String dest1, final String dest2
+            , final String dest3) {
+        return new SchemaRequest.AddCopyField(source, List.of(dest1, dest2, dest3));
+    }
+
+    public static SchemaRequest.AddDynamicField addDynamicField(final String name, final String type,
+            final boolean indexed, final boolean stored) {
+        return new SchemaRequest.AddDynamicField(
+                Map.of(NAME, name,
+                       TYPE, type,
+                       INDEXED, indexed,
+                       STORED, stored
+                ));
+    }
+
     public static SchemaRequest.DeleteField deleteField(final String name) {
         return new SchemaRequest.DeleteField(name);
     }
 
-//    protected void executeGenericSolrRequest(final String command) throws SolrServerException, IOException {
+    //    protected void executeGenericSolrRequest(final String command) throws SolrServerException, IOException {
 //        final var request = new GenericSolrRequest(SolrRequest.METHOD.POST, "/config", null);
 //        final var content = new RequestWriter.StringPayloadContentWriter(command, MediaType.APPLICATION_JSON);
 //        request.setContentWriter(content);
