14e752041117c6b460f478c20c4878ee575b7b31
Andy Verberne <andy.verberne@atos.net>
2022-11-29 10:50:56 +0100

    zaakafhandelcomponent-1896 Use of Optional in findXXX() service methods - in net.atos.client package

--- src/main/java/net/atos/zac/flowable/TaskVariablesService.java
+++ src/main/java/net/atos/zac/flowable/TaskVariablesService.java
@@ -7,6 +7,7 @@ package net.atos.zac.flowable;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -33,24 +34,27 @@ public class TaskVariablesService {
     @Inject
     private CmmnHistoryService cmmnHistoryService;
 
-    public Map<String, String> findTaakdata(final String taskId) {
-        return (Map<String, String>) findTaskVariable(taskId, VAR_TAAKDATA);
+    public Optional<Map<String, String>> findTaakdata(final String taskId) {
+        final var taakdata = findTaskVariable(taskId, VAR_TAAKDATA);
+        return taakdata != null ? Optional.of((Map<String, String>) taakdata) : Optional.empty();
     }
 
     public void setTaakdata(final String taskId, final Map<String, String> taakdata) {
         setTaskVariable(taskId, VAR_TAAKDATA, taakdata);
     }
 
-    public Map<String, String> findTaakinformatie(final String taskId) {
-        return (Map<String, String>) findTaskVariable(taskId, VAR_TAAKINFORMATIE);
+    public Optional<Map<String, String>> findTaakinformatie(final String taskId) {
+        final var taakinformatie = findTaskVariable(taskId, VAR_TAAKINFORMATIE);
+        return taakinformatie != null ? Optional.of((Map<String, String>) taakinformatie) : Optional.empty();
     }
 
     public void setTaakinformatie(final String taskId, final Map<String, String> taakinformatie) {
         setTaskVariable(taskId, VAR_TAAKINFORMATIE, taakinformatie);
     }
 
-    public List<UUID> findTaakdocumenten(final String taskId) {
-        return (List<UUID>) findTaskVariable(taskId, VAR_TAAKDOCUMENTEN);
+    public Optional<List<UUID>> findTaakdocumenten(final String taskId) {
+        final var taakdocumenten = findTaskVariable(taskId, VAR_TAAKDOCUMENTEN);
+        return taakdocumenten != null ? Optional.of((List<UUID>) taakdocumenten) : Optional.empty();
     }
 
     public void setTaakdocumenten(final String taskId, final List<UUID> taakdocumenten) {
