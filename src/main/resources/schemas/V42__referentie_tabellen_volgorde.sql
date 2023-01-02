a3a3f08e5dc1225c7ab61c4c5723c7bb54377bd1
Hans <hans.marks@atos.net>
2023-01-02 13:06:03 +0100

    zaakafhandelcomponent-1854 Geen sortering in rijen van de referentietabellen

--- src/main/resources/schemas/V42__referentie_tabellen_volgorde.sql
+++ src/main/resources/schemas/V42__referentie_tabellen_volgorde.sql
@@ -3,5 +3,5 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 ALTER TABLE ${schema}.referentie_waarde
-    ADD COLUMN volorde INT NOT NULL DEFAULT (0);
+    ADD COLUMN volgorde INT NOT NULL DEFAULT (0);
 CREATE INDEX ix_referentie_waarde_volgorde ON ${schema}.referentie_waarde USING btree (volgorde);
