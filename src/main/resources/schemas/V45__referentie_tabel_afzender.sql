5c79544fc586ee04c809ff1de2d4408a9fde13ad
Hans <hans.marks@atos.net>
2023-01-23 10:49:10 +0100

    zaakafhandelcomponent-2096 Mail versturen met flexibele keuze afzender emailadres

--- src/main/resources/schemas/V45__referentie_tabel_afzender.sql
+++ src/main/resources/schemas/V45__referentie_tabel_afzender.sql
@@ -3,4 +3,4 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 INSERT INTO ${schema}.referentie_tabel(id_referentie_tabel, code, naam)
-VALUES (1, 'AFZENDER', 'Afzender');
+VALUES (NEXTVAL('sq_referentie_tabel'), 'AFZENDER', 'Afzender');
