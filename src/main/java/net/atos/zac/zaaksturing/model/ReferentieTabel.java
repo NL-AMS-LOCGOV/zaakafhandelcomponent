f2ac198ecf0851203874d31b91242a6f1a841cc7
Hans <hans.marks@atos.net>
2023-01-23 10:46:03 +0100

    zaakafhandelcomponent-2096 Mail versturen met flexibele keuze afzender emailadres

--- src/main/java/net/atos/zac/zaaksturing/model/ReferentieTabel.java
+++ src/main/java/net/atos/zac/zaaksturing/model/ReferentieTabel.java
@@ -33,6 +33,7 @@ public class ReferentieTabel {
 
     public enum Systeem {
         ADVIES,
+        AFZENDER,
         DOMEIN
     }
 
---
+++ src/main/resources/schemas/V45__referentie_tabel_afzender.sql
@@ -0,0 +1,6 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Atos
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+INSERT INTO ${schema}.referentie_tabel(id_referentie_tabel, code, naam)
+VALUES (1, 'AFZENDER', 'Afzender');
