0023a204877dd99ecac8261cfe8bc7184e6d7115
Jeroen Dekker <jeroen.dekker@atos.net>
2023-02-21 12:24:40 +0100

    zaakafhandelcomponent-2311 Subjectize showAssignToMe

--- src/main/app/src/app/shared/edit/edit-groep-behandelaar/edit-groep-behandelaar.component.ts
+++ src/main/app/src/app/shared/edit/edit-groep-behandelaar/edit-groep-behandelaar.component.ts
@@ -3,27 +3,29 @@
  * SPDX-License-Identifier: EUPL-1.2+
  */
 
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {MaterialFormBuilderService} from '../../material-form-builder/material-form-builder.service';
 import {UtilService} from '../../../core/service/util.service';
 import {InputFormField} from '../../material-form-builder/form-components/input/input-form-field';
 import {IdentityService} from '../../../identity/identity.service';
-import {User} from '../../../identity/model/user';
 import {EditComponent} from '../edit.component';
 import {MedewerkerGroepFormField} from '../../material-form-builder/form-components/medewerker-groep/medewerker-groep-form-field';
 import {FormControl} from '@angular/forms';
+import {LoggedInUser} from '../../../identity/model/logged-in-user';
+import {Subject, Subscription} from 'rxjs';
 
 @Component({
     selector: 'zac-edit-groep-behandelaar',
     templateUrl: './edit-groep-behandelaar.component.html',
     styleUrls: ['../../static-text/static-text.component.less', '../edit.component.less', './edit-groep-behandelaar.component.less']
 })
-export class EditGroepBehandelaarComponent extends EditComponent implements OnInit {
+export class EditGroepBehandelaarComponent extends EditComponent implements OnInit, OnDestroy {
 
     @Input() formField: MedewerkerGroepFormField;
     @Input() reasonField: InputFormField;
-    showAssignToMe: boolean = false;
-    private loggedInUser: User;
+    showAssignToMe = new Subject<boolean>();
+    groepSubscription: Subscription;
+    private loggedInUser: LoggedInUser;
 
     constructor(mfbService: MaterialFormBuilderService, utilService: UtilService, private identityService: IdentityService) {
         super(mfbService, utilService);
@@ -32,10 +34,20 @@ export class EditGroepBehandelaarComponent extends EditComponent implements OnIn
         });
     }
 
+    ngOnInit(): void {
+        super.ngOnInit();
+        this.groepSubscription = this.formField.groep.valueChanges.subscribe(_ => this.updateShowAssignToMe());
+    }
+
+    ngOnDestroy(): void {
+        super.ngOnDestroy();
+        this.groepSubscription.unsubscribe();
+    }
+
     edit(): void {
         super.edit();
 
-        this.showAssignToMe = this.loggedInUser.id !== this.formField.medewerker.value?.id;
+        this.updateShowAssignToMe();
         if (this.reasonField) {
             this.formFields.setControl('reden', this.reasonField.formControl);
         }
@@ -51,5 +63,10 @@ export class EditGroepBehandelaarComponent extends EditComponent implements OnIn
 
     getFormControl(control: string): FormControl {
         return this.formField.formControl.get(control) as FormControl;
+    }
+
+    updateShowAssignToMe(): void {
+        this.showAssignToMe.next(this.loggedInUser.id !== this.formField.medewerker.value?.id &&
+            this.loggedInUser.groupIds.indexOf(this.formField.groep.value.id) >= 0);
     }
 }
