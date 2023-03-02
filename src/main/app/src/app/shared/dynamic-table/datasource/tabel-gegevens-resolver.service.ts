feb2879908c19429b4c323f9154f7d72b29194a3
Ronnie Koopmans <ronnie.koopmans@atos.net>
2023-03-02 15:11:31 +0100

    zaakafhandelcomponent-2215 OLDENZAAL: Aantal items per pagina instelling vasthouden cru(d)

--- src/main/app/src/app/shared/dynamic-table/datasource/tabel-gegevens-resolver.service.ts
+++ src/main/app/src/app/shared/dynamic-table/datasource/tabel-gegevens-resolver.service.ts
@@ -8,8 +8,6 @@ import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/rou
 import {Observable} from 'rxjs';
 import {TabelGegevens} from '../model/tabel-gegevens';
 import {GebruikersvoorkeurenService} from '../../../gebruikersvoorkeuren/gebruikersvoorkeuren.service';
-import {Werklijst} from '../../../gebruikersvoorkeuren/model/werklijst';
-import {ZakenWerkvoorraadComponent} from '../../../zaken/zaken-werkvoorraad/zaken-werkvoorraad.component';
 
 @Injectable({
     providedIn: 'root'
@@ -19,28 +17,7 @@ export class TabelGegevensResolver implements Resolve<TabelGegevens> {
     }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<TabelGegevens> {
-        return this.gebruikersvoorkeurenService.readTabelGegevens(this.getWerklijst(route.component.name));
-    }
-
-    getWerklijst(component: string): Werklijst {
-        switch (component) {
-            case 'ZakenWerkvoorraadComponent':
-                return Werklijst.WERKVOORRAAD_ZAKEN;
-            case 'ZakenMijnComponent':
-                return Werklijst.MIJN_ZAKEN;
-            case 'ZakenAfgehandeldComponent':
-                return Werklijst.AFGEHANDELDE_ZAKEN;
-            case 'TakenWerkvoorraadComponent':
-                return Werklijst.WERKVOORRAAD_TAKEN;
-            case 'TakenMijnComponent':
-                return Werklijst.MIJN_TAKEN;
-            case 'OntkoppeldeDocumentenListComponent':
-                return Werklijst.ONTKOPPELDE_DOCUMENTEN;
-            case 'InboxDocumentenListComponent':
-                return Werklijst.INBOX_DOCUMENTEN;
-            default:
-                throw Error('Geen werklijst gevonden voor ' + component);
-        }
+        return this.gebruikersvoorkeurenService.readTabelGegevens(route.data.werklijst);
     }
 
 }
