6bda1941ee0b2944c67e4e9dac76055e4cd39ed8
nl22202 <hans.marks@atos.net>
2023-05-23 16:21:15 +0200

    zaakafhandelcomponent-2449 BUG: Afhandeling wanneer het zaaktype in O-Z niet het informatieobject...

--- src/main/java/net/atos/client/or/shared/exception/ValidatieFoutException.java
+++ src/main/java/net/atos/client/or/shared/exception/ValidatieFoutException.java
@@ -5,6 +5,9 @@
 
 package net.atos.client.or.shared.exception;
 
+import java.net.URI;
+import java.util.stream.Collectors;
+
 import net.atos.client.or.shared.model.ValidatieFout;
 
 /**
@@ -24,6 +27,21 @@ public class ValidatieFoutException extends RuntimeException {
 
     @Override
     public String getMessage() {
-        return validatieFout.toString();
+        return "[%d] %s %s %s %s: %s"
+                .formatted(validatieFout.getStatus(),
+                           validatieFout.getCode(),
+                           validatieFout.getTitle(),
+                           validatieFout.getDetail(),
+                           uri(validatieFout.getInstance()),
+                           validatieFout.getFieldValidationErrors().stream()
+                                   .map(error -> "%s %s %s"
+                                           .formatted(error.getCode(),
+                                                      error.getName(),
+                                                      error.getReason()))
+                                   .collect(Collectors.joining(", ")));
+    }
+
+    private String uri(final URI uri) {
+        return uri == null ? null : uri.toString();
     }
 }
