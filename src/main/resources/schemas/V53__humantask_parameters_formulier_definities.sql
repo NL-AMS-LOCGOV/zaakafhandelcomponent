e9622241a8f48516a6f11c339439153e311328c2
Ronnie Koopmans <ronnie.koopmans@atos.net>
2023-06-21 09:48:25 +0200

    zaakafhandelcomponent-2579 Formulierdefinitie koppelen aan taken uit het cmmn proces

---
+++ src/main/resources/schemas/V53__humantask_parameters_formulier_definities.sql
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Atos
+ * SPDX-License-Identifier: EUPL-1.2+
+ */
+
+ALTER TABLE ${schema}.humantask_parameters
+    ADD COLUMN id_start_formulier_definition VARCHAR;
+
+ALTER TABLE ${schema}.humantask_parameters
+    ADD COLUMN id_behandel_formulier_definition VARCHAR;
+
+CREATE INDEX idx_humantask_parameters_id_start_formulier_definition
+    ON ${schema}.humantask_parameters USING btree (id_start_formulier_definition);
+
+CREATE INDEX idx_humantask_parameters_id_behandel_formulier_definition
+    ON ${schema}.humantask_parameters USING btree (id_behandel_formulier_definition);
