3f2a86624d3b927f3d3f3ef3a892ce013417431d
Ronnie Koopmans <ronnie.koopmans@atos.net>
2023-06-22 09:46:15 +0200

    zaakafhandelcomponent-2579 Formulierdefinitie koppelen aan taken uit het cmmn proces

--- src/main/resources/schemas/V54__formulier_definitie_mail_versturen.sql
+++ src/main/resources/schemas/V54__formulier_definitie_mail_versturen.sql
@@ -6,8 +6,8 @@
 ALTER TABLE ${schema}.formulier_definitie
     ADD COLUMN mail_versturen BOOLEAN,
     ADD COLUMN mail_to VARCHAR,
-    ADD COLUMN mail_to VARCHAR,
     ADD COLUMN mail_from VARCHAR,
+    ADD COLUMN mail_subject VARCHAR,
     ADD COLUMN mail_body VARCHAR,
     ADD COLUMN mail_cc VARCHAR,
     ADD COLUMN mail_bcc VARCHAR;
