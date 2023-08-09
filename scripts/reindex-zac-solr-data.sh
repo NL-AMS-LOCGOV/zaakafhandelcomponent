e634b3ba39fc27bdb345ff53425a94cd2a7064f7
Edgar Vonk <edgar@info.nl>
2023-08-09 13:19:15 +0200

    feat: added simple script to manually reindex ZAC Solr data

--- scripts/reindex-zac-solr-data.sh
+++ scripts/reindex-zac-solr-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Send requests ZAC to reindex zaak, taak and document data (in Solr). This assumes a locally running ZAC instance on the default port 8080."
+echo "Send requests to ZAC to reindex zaak, taak and document data (in Solr). This assumes a locally running ZAC instance on the default port 8080."
 
 ## Note that we first need to mark all items for re-indexation per item type
 curl http://localhost:8080/rest/indexeren/herindexeren/ZAAK \
